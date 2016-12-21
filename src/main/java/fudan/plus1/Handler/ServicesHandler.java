2d28ba3e1876f1ed259cbf1454878ce0a5b83703
WenxingLai <laibill@icloud.com>
2016-12-21 09:39:24 +0800

    added JSONP support

--- src/main/java/fudan/plus1/Handler/ServicesHandler.java
+++ src/main/java/fudan/plus1/Handler/ServicesHandler.java
@@ -7,6 +7,8 @@ import fudan.plus1.JsonTypes.UserExistence;
 import fudan.plus1.Kits.Finals;
 
 import org.codehaus.jettison.json.JSONObject;
+import org.glassfish.jersey.server.JSONP;
+
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.*;
 
@@ -16,8 +18,9 @@ import javax.ws.rs.*;
 @Path("/services")
 public class ServicesHandler {
     @Path("/userexistence/{username}")
+    @JSONP
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
     public UserExistence getUserExistence(@PathParam("username") String username) {
         boolean isUserNameAvailable = Plus1System.getInstance().isUsernameAvailable(username);
         String existence = "" + (isUserNameAvailable ? "yes" : "no");
@@ -27,7 +30,8 @@ public class ServicesHandler {
 
     @Path("/verification/{username}/{password}")
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @JSONP
+    @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
     public Result getVerification(@PathParam("username") String username, @PathParam("password") String password) {
         if (username == null || password == null) {
             return new Result(Finals.INFO_MISSING);
@@ -38,8 +42,9 @@ public class ServicesHandler {
 
     @Path("/countercreation")
     @POST
+    @JSONP
+    @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Result postCounterCreation(JSONObject json) {
         String administrator = null;
         String counterName = null;
@@ -72,7 +77,8 @@ public class ServicesHandler {
     @Path("/usercreation")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+    @JSONP
+    @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
     public Result postUserCreation(JSONObject json) {
         String username = null;
         String password = null;
